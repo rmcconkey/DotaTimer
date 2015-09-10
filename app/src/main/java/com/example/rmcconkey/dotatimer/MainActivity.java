e4ed963fd4245015cf60b09cc51d7b5268037466
Rob McConkey <rmcconkey@careered.com>
2015-09-10 14:20:53 -0500

    - now saves timer values and switch settings in SharedPreferences

--- app/src/main/java/com/example/rmcconkey/dotatimer/MainActivity.java
+++ app/src/main/java/com/example/rmcconkey/dotatimer/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
@@ -12,6 +13,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.NumberPicker;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -32,10 +34,32 @@ public class MainActivity extends Activity implements NumberPicker.OnValueChange
     private int minutes;
     private int seconds;
 
+    private int neutralAlertTime;
+    private int runeAlertTime;
+    private int roshanAlertTime;
+    private int aegisAlertTime;
+
     private boolean neutralAlertEnabled = true;
     private boolean runeAlertEnabled = true;
     private boolean roshanAlertEnabled = true;
     private boolean aegisAlertEnabled = true;
+
+    private Switch neutralCampSwitch;
+    private Switch runeSwitch;
+    private Switch roshanSwitch;
+    private Switch aegisSwitch;
+
+    SharedPreferences prefs;
+    private static final String PREFERENCES = "dota timer";
+
+    private static final String NEUTRAL_SWITCH = "neutral switch";
+    private static final String RUNE_SWITCH = "rune switch";
+    private static final String ROSHAN_SWITCH = "roshan switch";
+    private static final String AEGIS_SWITCH = "aegis switch";
+    private static final String NEUTRAL_ALERT_TIME = "neutral alert time";
+    private static final String RUNE_ALERT_TIME = "rune alert time";
+    private static final String ROSHAN_ALERT_TIME = "roshan alert time";
+    private static final String AEGIS_ALERT_TIME = "aegis alert time";
 
     private Handler timerHandler = new Handler();
     private Runnable timerRunnable = new Runnable() {
@@ -56,6 +80,11 @@ public class MainActivity extends Activity implements NumberPicker.OnValueChange
         mainClockMinutes = (TextView)findViewById(R.id.mainClockMinutes);
         mainClockSeconds = (TextView)findViewById(R.id.mainClockSeconds);
 
+        neutralCampSwitch = (Switch)findViewById(R.id.neutralCampSwitch);
+        runeSwitch = (Switch)findViewById(R.id.runeSwitch);
+        roshanSwitch = (Switch)findViewById(R.id.roshanSwitch);
+        aegisSwitch = (Switch)findViewById(R.id.aegisSwitch);
+
         syncButton = (Button)findViewById(R.id.syncButton);
         syncButton.setOnClickListener(new View.OnClickListener() {
 
@@ -72,6 +101,27 @@ public class MainActivity extends Activity implements NumberPicker.OnValueChange
             }
         });
 
+        prefs = getSharedPreferences(PREFERENCES, Context.MODE_PRIVATE);
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        loadSettings();
+    }
+
+    private void loadSettings() {
+        neutralAlertTime = prefs.getInt(NEUTRAL_ALERT_TIME, 0);
+        runeAlertTime = prefs.getInt(RUNE_ALERT_TIME, 0);
+        roshanAlertTime = prefs.getInt(ROSHAN_ALERT_TIME, 0);
+        aegisAlertTime = prefs.getInt(AEGIS_ALERT_TIME, 0);
+
+        neutralCampSwitch.setChecked(prefs.getBoolean(NEUTRAL_SWITCH, false));
+        runeSwitch.setChecked(prefs.getBoolean(RUNE_SWITCH, false));
+        roshanSwitch.setChecked(prefs.getBoolean(ROSHAN_SWITCH, false));
+        aegisSwitch.setChecked(prefs.getBoolean(AEGIS_SWITCH, false));
     }
 
     @Override
@@ -79,6 +129,25 @@ public class MainActivity extends Activity implements NumberPicker.OnValueChange
         super.onPause();
         timerHandler.removeCallbacks(timerRunnable);
         syncButton.setText("Sync");
+
+        saveSettings();
+    }
+
+    private void saveSettings() {
+        SharedPreferences.Editor editor = prefs.edit();
+
+        editor.putInt(NEUTRAL_ALERT_TIME, neutralAlertTime);
+        editor.putInt(RUNE_ALERT_TIME, runeAlertTime);
+        editor.putInt(ROSHAN_ALERT_TIME, roshanAlertTime);
+        editor.putInt(AEGIS_ALERT_TIME, aegisAlertTime);
+
+        editor.putBoolean(NEUTRAL_SWITCH, neutralCampSwitch.isChecked());
+        editor.putBoolean(RUNE_SWITCH, runeSwitch.isChecked());
+        editor.putBoolean(ROSHAN_SWITCH, roshanSwitch.isChecked());
+        editor.putBoolean(AEGIS_SWITCH, aegisSwitch.isChecked());
+
+        editor.apply();
+
     }
 
     @Override
@@ -150,6 +219,7 @@ public class MainActivity extends Activity implements NumberPicker.OnValueChange
                 break;
             default :
                 Toast.makeText(this, "Error: view.getId() not found", Toast.LENGTH_LONG).show();
+                Log.e(TAG, "Error: view.getId() not found");
                 title = "default title";
                 timeValueType = null;
         }
